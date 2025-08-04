bc09c0bd49405a028231b7fe15163efd8af95b61
Secret G Spot <secregspot@gmail.com>
2025-08-04 01:15:32 -0600

    cleanup

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,10 @@
-import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: { adapter: adapter() },
-	preprocess: [mdsvex({
-		extensions: ['.svx'],
-		layout: {
-			_: './src/lib/layouts/mdsvex.svelte'
-		}
-	})],
-	extensions: ['.svelte', '.svx']
+	preprocess: [],
+	extensions: ['.svelte']
 };
 
 export default config;
